57254bb4fada93765900fa6215f6f6ea9d69b0c6
Megh39 <meghnanavati1@gmail.com>
2024-09-26 16:21:17 +0530

    Client path updated

--- src/Components/OutleadClients/ClientsData.jsx
+++ src/Components/OutleadClients/ClientsData.jsx
@@ -1,262 +1,262 @@
 const Client = [
     {
-        img: "../../public/OutleadClientLogos/prepseed.png",
+        img: "/OutleadClientLogos/prepseed.png",
     },
     {
-        img: "../../public/OutleadClientLogos/chartered.png",
+        img: "/OutleadClientLogos/chartered.png",
     },
     {
-        img: "../../public/OutleadClientLogos/craftezy.png",
+        img: "/OutleadClientLogos/craftezy.png",
     },
     {
-        img: "../../public/OutleadClientLogos/shivtex.png",
+        img: "/OutleadClientLogos/shivtex.png",
     },
     {
-        img: "../../public/OutleadClientLogos/rdt.png",
+        img: "/OutleadClientLogos/rdt.png",
     },
     {
-        img: "../../public/OutleadClientLogos/cidroy.png",
+        img: "/OutleadClientLogos/cidroy.png",
     },
 
     {
-        img: "../../public/OutleadClientLogos/adira.png",
+        img: "/OutleadClientLogos/adira.png",
     },
     {
-        img: "../../public/OutleadClientLogos/drashti.png",
+        img: "/OutleadClientLogos/drashti.png",
     },
     {
-        img: "../../public/OutleadClientLogos/FA.png",
+        img: "/OutleadClientLogos/FA.png",
     },
     {
-        img: "../../public/OutleadClientLogos/Bim.png",
+        img: "/OutleadClientLogos/Bim.png",
     },
     {
-        img: "../../public/OutleadClientLogos/cardinal.png",
+        img: "/OutleadClientLogos/cardinal.png",
     },
     {
-        img: '../../public/OutleadClientLogos/digvijay.png',
+        img: '/OutleadClientLogos/digvijay.png',
     },
     {
-        img: "../../public/OutleadClientLogos/64.png",
+        img: "/OutleadClientLogos/64.png",
     },
     {
-        img: "../../public/OutleadClientLogos/adrija.png",
+        img: "/OutleadClientLogos/adrija.png",
     },
     {
-        img: "../../public/OutleadClientLogos/age.png",
+        img: "/OutleadClientLogos/age.png",
     },
     {
-        img: "../../public/OutleadClientLogos/archbude.png",
+        img: "/OutleadClientLogos/archbude.png",
     }
     ,
     {
-        img: "../../public/OutleadClientLogos/bbg.png",
+        img: "/OutleadClientLogos/bbg.png",
     },
     {
-        img: "../../public/OutleadClientLogos/beacon.png",
+        img: "/OutleadClientLogos/beacon.png",
     },
     {
-        img: "../../public/OutleadClientLogos/biochem.png",
+        img: "/OutleadClientLogos/biochem.png",
     },
     {
-        img: "../../public/OutleadClientLogos/bluefire.png",
+        img: "/OutleadClientLogos/bluefire.png",
     },
     {
-        img: "../../public/OutleadClientLogos/Bsquare.png",
+        img: "/OutleadClientLogos/Bsquare.png",
     }
     ,
     {
-        img: "../../public/OutleadClientLogos/hitech.png",
+        img: "/OutleadClientLogos/hitech.png",
     },
     {
-        img: "../../public/OutleadClientLogos/ninefive.png",
+        img: "/OutleadClientLogos/ninefive.png",
     },
     {
-        img: "../../public/OutleadClientLogos/qutone.png",
+        img: "/OutleadClientLogos/qutone.png",
     },
     {
-        img: "../../public/OutleadClientLogos/book.png",
+        img: "/OutleadClientLogos/book.png",
     },
     {
-        img: "../../public/OutleadClientLogos/braincuber.png",
+        img: "/OutleadClientLogos/braincuber.png",
     },
     {
-        img: "../../public/OutleadClientLogos/codertrails.png",
+        img: "/OutleadClientLogos/codertrails.png",
     },
 
     // {
-    //   img: "../../public/OutleadClientLogos/bsquare.png",
+    //   img: "/OutleadClientLogos/bsquare.png",
     // },
     {
-        img: "../../public/OutleadClientLogos/enable.png",
+        img: "/OutleadClientLogos/enable.png",
     },
     {
-        img: "../../public/OutleadClientLogos/estrella.png"
+        img: "/OutleadClientLogos/estrella.png"
     },
     {
-        img: "../../public/OutleadClientLogos/finanvo.png",
+        img: "/OutleadClientLogos/finanvo.png",
     },
     {
-        img: "../../public/OutleadClientLogos/gloline.png",
+        img: "/OutleadClientLogos/gloline.png",
     },
     {
-        img: "../../public/OutleadClientLogos/gramvaani.png",
+        img: "/OutleadClientLogos/gramvaani.png",
     },
     {
-        img: "../../public/OutleadClientLogos/H4Healing.jpg",
+        img: "/OutleadClientLogos/H4Healing.jpg",
     },
     {
-        img: "../../public/OutleadClientLogos/happick.png",
+        img: "/OutleadClientLogos/happick.png",
     },
     {
-        img: "../../public/OutleadClientLogos/inspire.png",
+        img: "/OutleadClientLogos/inspire.png",
     },
     {
-        img: "../../public/OutleadClientLogos/maak.png",
+        img: "/OutleadClientLogos/maak.png",
     },
     {
-        img: "../../public/OutleadClientLogos/mandir.png",
+        img: "/OutleadClientLogos/mandir.png",
     },
     {
-        img: "../../public/OutleadClientLogos/mastersoft.png",
+        img: "/OutleadClientLogos/mastersoft.png",
     },
     {
-        img: "../../public/OutleadClientLogos/online.png",
+        img: "/OutleadClientLogos/online.png",
     },
     {
-        img: "../../public/OutleadClientLogos/pet.png",
+        img: "/OutleadClientLogos/pet.png",
     },
     {
-        img: "../../public/OutleadClientLogos/poas.png",
+        img: "/OutleadClientLogos/poas.png",
     },
     {
-        img: "../../public/OutleadClientLogos/savory.png",
+        img: "/OutleadClientLogos/savory.png",
     },
     {
-        img: "../../public/OutleadClientLogos/scones.png",
+        img: "/OutleadClientLogos/scones.png",
     },
     {
-        img: "../../public/OutleadClientLogos/shinkhan.png",
+        img: "/OutleadClientLogos/shinkhan.png",
     },
     {
-        img: "../../public/OutleadClientLogos/simplso.png",
+        img: "/OutleadClientLogos/simplso.png",
     },
     {
-        img: "../../public/OutleadClientLogos/softco.png",
+        img: "/OutleadClientLogos/softco.png",
     },
     {
-        img: "../../public/OutleadClientLogos/techmyntra.png",
+        img: "/OutleadClientLogos/techmyntra.png",
     },
     {
-        img: "../../public/OutleadClientLogos/v.png",
+        img: "/OutleadClientLogos/v.png",
     },
     {
-        img: "../../public/OutleadClientLogos/valuemize.png",
+        img: "/OutleadClientLogos/valuemize.png",
     },
     {
-        img: "../../public/OutleadClientLogos/KhushiLogo.JPG",
+        img: "/OutleadClientLogos/KhushiLogo.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Neutec.JPG",
+        img: "/OutleadClientLogos/Neutec.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/FR.JPG",
+        img: "/OutleadClientLogos/FR.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Divya.JPG",
+        img: "/OutleadClientLogos/Divya.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/TDM.JPG",
+        img: "/OutleadClientLogos/TDM.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Vebbbeeeee.JPG",
+        img: "/OutleadClientLogos/Vebbbeeeee.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/SUMS.JPG",
+        img: "/OutleadClientLogos/SUMS.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/DYD.JPG",
+        img: "/OutleadClientLogos/DYD.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/ClarityComm.JPG",
+        img: "/OutleadClientLogos/ClarityComm.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/AB Mittal.JPG",
+        img: "/OutleadClientLogos/AB Mittal.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/EximiusVenture.JPG",
+        img: "/OutleadClientLogos/EximiusVenture.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Fiable.JPG",
+        img: "/OutleadClientLogos/Fiable.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/yellowspot.JPG",
+        img: "/OutleadClientLogos/yellowspot.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Tribalvedda.JPG",
+        img: "/OutleadClientLogos/Tribalvedda.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Growth91.JPG",
+        img: "/OutleadClientLogos/Growth91.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/ITZEL.JPG",
+        img: "/OutleadClientLogos/ITZEL.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Tringular.JPG",
+        img: "/OutleadClientLogos/Tringular.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Orget.JPG",
+        img: "/OutleadClientLogos/Orget.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/ACE racquets.JPG",
+        img: "/OutleadClientLogos/ACE racquets.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Happick.JPG",
+        img: "/OutleadClientLogos/Happick.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Gloline.JPG",
+        img: "/OutleadClientLogos/Gloline.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Club.JPG",
+        img: "/OutleadClientLogos/Club.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/RMJ.JPG",
+        img: "/OutleadClientLogos/RMJ.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Advisory.JPG",
+        img: "/OutleadClientLogos/Advisory.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Tikkaow.JPG",
+        img: "/OutleadClientLogos/Tikkaow.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Vibfast.JPG",
+        img: "/OutleadClientLogos/Vibfast.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/AlmaShines.JPG",
+        img: "/OutleadClientLogos/AlmaShines.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/quickDraft.JPG",
+        img: "/OutleadClientLogos/quickDraft.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Proffestional Edge.JPG",
+        img: "/OutleadClientLogos/Proffestional Edge.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/H4Healing.JPG",
+        img: "/OutleadClientLogos/H4Healing.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Soul.JPG",
+        img: "/OutleadClientLogos/Soul.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Hyrespree.JPG",
+        img: "/OutleadClientLogos/Hyrespree.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/PBI.JPG",
+        img: "/OutleadClientLogos/PBI.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/Tronsoft.JPG",
+        img: "/OutleadClientLogos/Tronsoft.JPG",
     },
     {
-        img: "../../public/OutleadClientLogos/risentech.JPG",
+        img: "/OutleadClientLogos/risentech.JPG",
     },
 
 
