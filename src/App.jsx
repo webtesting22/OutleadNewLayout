0271b89df1a7e5a906fd8cbc6e78c1f3945d2049
webtesting22 <webtesting2232@gmail.com>
2024-09-27 12:14:10 +0530

    Updates routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AgiluxHome from './Components/AgiluxHome/AgiluxHome';
 import './App.css';
 import AgiluxNavigation from './Components/AgiluxNavigation/AgiluxNavigation';
-import AppDevelopment from './Components/MinimalComponent/AppDevelopmentProjects/AppDevelopment';
-import WebDevelopment from './Components/MinimalComponent/WebDevelopmentProjects/WebDevelopment';
 import OutleadAboutUs from './Components/AboutUs/OutleadAboutUs';
 import OutleadFooter from './Components/OutleadFooter/OutleadFooter';
 import OutleadClients from './Components/OutleadClients/OutleadClients';
@@ -15,8 +13,6 @@ function App() {
 
         <Route exact path="/" element={<AgiluxHome />} />
         <Route exact path="/outleadabout" element={<OutleadAboutUs />} />
-        <Route exact path="/appdevelopment" element={<AppDevelopment />} />
-        <Route exact path="/webdevelopment" element={<WebDevelopment />} />
         <Route exact path="/outleadclients" element={<OutleadClients />} />
       </Routes>
       <OutleadFooter/>
