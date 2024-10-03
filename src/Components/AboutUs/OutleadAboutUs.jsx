8317bac7ea6d8f4925927a316ba9942d23a59b7f
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2024-10-03 17:02:52 +0530

    Update OutleadAboutUs.jsx

--- src/Components/AboutUs/OutleadAboutUs.jsx
+++ src/Components/AboutUs/OutleadAboutUs.jsx
@@ -65,15 +65,7 @@ const OutleadAboutUs = () => {
         {
             container: ""
         },
-        {
-            container: ""
-        },
-        {
-            container: ""
-        },
-        {
-            container: ""
-        },
+        
 
     ]
 
@@ -206,39 +198,6 @@ const OutleadAboutUs = () => {
                             </Row>
                         </div>
                     </div>
-                    {/* <div className="TopContainer">
-                        <h4 style={{ position: "sticky" }} className="Slashaheading">/ About Us</h4>
-                        <div style={{ display: "flex", justifyContent: "space-Between" }}>
-                            <h2>Outlead About</h2>
-                        </div>
-                    </div> */}
-                    {/* <div className="TopContainer">
-                        <div className="BlackTopOverlay">
-
-                        </div>
-                        <h4 style={{position:"sticky"}} className="Slashaheading">/ About Us</h4>
-                        <div style={{ display: "flex", justifyContent: "space-Between" }}>
-                            <h2>Outlead About</h2>
-                        </div>
-                    </div>
-                    <div className="AboutInfoContainer">
-                        <p>We are an innovative marketing company, dedicated to helping businesses succeed through tailored strategies and captivating campaigns.</p>
-                        <br />
-                        <h4 className="Slashaheading">/ Outlead Solution</h4>
-                        <p>Outlead Solutions is a premier graphics design, marketing, and web development company, dedicated to helping businesses grow their online presence. We specialize in crafting visually stunning designs, innovative marketing strategies, and high-performance websites tailored to your unique needs. Our expert team ensures optimal user experiences and drives measurable results, empowering your brand to succeed in the digital landscape.</p>
-                        <br />
-                        className="parentAbsoluteContainer"
-                            <h4 className="Slashaheading">/ What is Outlead Solution?</h4>
-                            <p>The onset of the global pandemic has drastically changed the way businesses survive and thrive. The founders of OutLead took this challenge as an opportunity with one simple goal - to reconstruct the pillars of sales and marketing in the digital world.</p>
-                            <br />
-                            <p>OutLead is a proud composer of a unique 5-stage process for lead generation, where special emphasis is laid on establishing trust and authenticity with clients over the internet. This process further includes researching and identifying key potential customer bases over LinkedIn, by engaging with them in a dialogue and thereby selling the idea of your product/service.
-                            </p>
-                            <br />
-                            <p>This process of lead generation is further accompanied with carefully designed digital marketing tools, which involve - establishing digital presence of your business on key social media platforms, targeted strategies to identify and grow followers on such platforms, designing and posting of informative content pertaining to the business on a regular basis and so on.</p>
-                            <br />
-                            <p>It's time to join hands; and lead together with OutLead!</p>
-                        </div>
-                    </div> */}
                 </div>
             </section>
         </>
